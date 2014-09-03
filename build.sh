eba1a902287c338aa0c541d2fcc31d21ff7ef780
Joe Desbonnet <jdesbonnet@gmail.com>
2014-09-03 22:26:01 +0100

    Try forcing Java source and target 1.7

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # TODO: replace with ANT make file
-mkdir bin
+if [ ! -e bin ] ; then
+	mkdir bin
+fi
 javac -classpath ./lib/dom4j-1.6.1.jar:./lib/jaxen-1.1.1.jar \
 	-sourcepath ./src \
 	-d ./bin \
+	-target 1.7 -source 1.7 \
 	./src/ie/wombat/icrdl/MakeCHeaders.java
 

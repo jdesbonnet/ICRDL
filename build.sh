4e07ff22201629e6f9a88ba62c77ac348b502b54
Joe Desbonnet <jdesbonnet@gmail.com>
2014-09-05 14:58:36 +0100

    no comment

--- build.sh
+++ build.sh
@@ -7,5 +7,6 @@ javac -classpath ./lib/dom4j-1.6.1.jar:./lib/jaxen-1.1.1.jar \
 	-sourcepath ./src \
 	-d ./bin \
 	-target 1.7 -source 1.7 \
-	./src/ie/wombat/icrdl/MakeCHeaders.java
+	./src/ie/wombat/icrdl/MakeCHeaders.java \
+	./src/ie/wombat/icrdl/UpdateCFile.java
 
---
+++ test/test.c
@@ -0,0 +1,14 @@
+#include "rfm69_rdl.h"
+
+void main (int argc, char **argv) {
+
+/* Do something
+ * @rdl RFM69.OPMODE.Mode
+ * START
+ * blah blah
+ * END	
+ */
+
+
+}
+
